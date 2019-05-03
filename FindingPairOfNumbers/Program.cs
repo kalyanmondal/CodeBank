c48121849688200feea3c9f6884c4ecdc0a9b99c
Mondal, Kalyan <kalyan.mondal19@gmail.com>
2019-05-03 12:07:47 +0530

    New Code

---
+++ FindingPairOfNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FindingPairOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int numberOfElementInTheArray = Convert.ToInt32(Console.ReadLine());
+            int[] mainArray = new int[numberOfElementInTheArray];
+
+            string[] temp = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < numberOfElementInTheArray; i++)
+            {
+                mainArray[i] = Convert.ToInt32(temp[i]);
+            }
+
+            int max = mainArray[0];
+
+            for (int j = 0; j < mainArray.Length; j++)
+            {
+                if (mainArray[j] > max)
+                {
+                    max = mainArray[j];
+                }
+            }
+
+            int[] subArray = new int[max];
+
+            for (int k = 0; k < mainArray.Length; k++)
+            {
+                subArray[mainArray[k] - 1] += 1;
+            }
+
+            for (int l = 0; l < subArray.Length; l++)
+            {
+                if (subArray[l] != 0)
+                {
+                    result += subArray[l] / 2;
+                }
+            }
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
